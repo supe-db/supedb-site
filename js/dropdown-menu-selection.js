cffadffdaa7c52c6eba2a615f8635f820c44b078
supe-db <214155371+supe-db@users.noreply.github.com>
2025-07-18 15:24:02 -0400

    Add new styling for character profile pages.

--- js/dropdown-menu-selection.js
+++ js/dropdown-menu-selection.js
@@ -7,6 +7,13 @@ const pageTitle = body.querySelector("h1").textContent;
 
 //Function to update the characters viewable on the screen based on series.
 function updateViewableCharacters(){
+
+  // Remove all dummy divs of class-type 'hidden-flex-item' before
+  // generating the current layout. More on this later in the function.
+  const dummyDivs = document.querySelectorAll('div.hidden-flex-item');
+  dummyDivs.forEach(div => div.remove());
+
+  //get the dropdown menu's current value
   const selectedValue = dropdown.value;
   console.log("Dropdown Value:", dropdown.value);
 
@@ -35,6 +42,20 @@ function updateViewableCharacters(){
 
   //Update characterTotal
   body.querySelector("h1").textContent = pageTitle + '(' + characterCount + ')';
+
+  //Generate dummy divs to fill in the last row so that flex-grow doesn't super 
+  //stretch items to fill the width of the screen. E.g. if a row has 2 items flex-
+  //grow will stretch them the whole width of a screen, this code ensures each row
+  //has at least 'maxItemsPerRow' which is the max number of items a normal row
+  //can have. Since each row is now full (b/c of the below loop), excessive stretching 
+  //won't happen.
+  const maxItemsPerRow = 6;
+  for(let i = 0; i < (maxItemsPerRow - (characterCount % maxItemsPerRow)); i++)
+  {
+    const newDummyDiv = document.createElement('div');
+    newDummyDiv.className = 'hidden-flex-item';
+    characters[characters.length-1].insertAdjacentElement('afterend', newDummyDiv); 
+  }
 
 }
 
